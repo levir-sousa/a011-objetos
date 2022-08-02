e84bc4e18842e5e4a1274cb405fb64966cd23b57
levir-sousa <levysouza2014@gmail.com>
2022-08-02 18:32:23 -0300

    Exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+//
+const sacolao = []
+
+// a)
+const arroz = {
+    nome: "Arroz",
+    preco: 15,
+    disponibilidade: true
+}
+const feijao = {
+    nome: "Feijão",
+    preco: 8,
+    disponibilidade: true
+}
+const pneu = {
+    nome: "Pneu",
+    preco: 400,
+    disponibilidade: false
+}
+
+// b)
+sacolao.push(arroz, feijao, pneu)
+
+// c)
+console.log("Lista de compras do sacolão:", sacolao);
